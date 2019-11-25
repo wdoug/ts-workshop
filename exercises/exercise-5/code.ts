9f501d4874d098fa861f4c7052302146c480b61d
Will Douglas <willygdouglas@gmail.com>
2019-11-25 13:34:28 -0700

    Complete exercise 5
    ...or at least get all the tests passing

--- exercises/exercise-5/code.ts
+++ exercises/exercise-5/code.ts
@@ -1,6 +1,59 @@
-import { Order } from "../exercise-3/code";
-import { ReceiptProps } from "../exercise-4/code";
+import { Order, LineItem } from "../exercise-3/code";
+import {
+  ReceiptProps,
+  ItemSummaryProps,
+  AdditionSummaryProps
+} from "../exercise-4/code";
+import { priceOrder, getLineItemPrice } from "../exercise-3/code";
+
+function getItemProps(lineItem: LineItem): ItemSummaryProps {
+  const additions: AdditionSummaryProps[] = [
+    {
+      additionType: lineItem.protein
+    }
+  ];
+  if (lineItem.type === "taco") {
+    if (lineItem.salsa) {
+      additions.push({ additionType: "salsa" });
+    }
+    if (lineItem.extraTaco) {
+      additions.push({ additionType: "extraTaco" });
+    }
+  } else if (lineItem.type === "sandwich") {
+    if (lineItem.protein === "portabelloCap") {
+      additions[0].annotation = "pricey";
+    }
+    if (lineItem.toppings.length > 0) {
+      lineItem.toppings.forEach(topping => {
+        additions.push({ additionType: topping });
+      });
+    }
+  } else if (lineItem.type === "sushi") {
+    if (lineItem.protein === "kingSalmon") {
+      additions[0].annotation = "pricey";
+    }
+    additions.push({ additionType: lineItem.riceType });
+  }
+  if (lineItem.awesomeSauce) {
+    additions.push({
+      additionType: "awesomeSauce",
+      annotation: "nuclear"
+    });
+  }
+  return {
+    entreeType: lineItem.type,
+    additions,
+    itemTotal: getLineItemPrice(lineItem),
+    mindBlowing: lineItem.awesomeSauce
+  };
+}
 
 export function orderToReceipt(order: Order, tip: number): ReceiptProps {
-  return null as any;
+  const subtotal = priceOrder(order);
+  return {
+    items: order.lineItems.map(getItemProps),
+    tip,
+    subtotal,
+    total: subtotal + tip
+  };
 }
