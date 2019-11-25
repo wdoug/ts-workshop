c2cd1f4699f2d83a4b735ba64a21e2558c2eab9c
Will Douglas <willygdouglas@gmail.com>
2019-11-25 13:30:57 -0700

    Complete exercise 3

--- exercises/exercise-3/code.ts
+++ exercises/exercise-3/code.ts
@@ -11,12 +11,12 @@ Taco....................$5
     Salsa (+$0.50)
   Add a second taco for +$3 (+$4 for Carnitas)
 Sushi ..................$8
-  Protein    
+  Protein
     Tuna
     King Salmon (+$2)
   Rice
     White Rice
-    Brown Rice (+$1) 
+    Brown Rice (+$1)
 Sandwich................$4
   Protein
     Chicken
@@ -53,15 +53,88 @@ export type RiceType =
   | "brownRice" // 🍘
   | "whiteRice"; // 🍙
 
+type Taco = {
+  type: "taco";
+  protein:
+    | "chicken" // 🐓
+    | "jackfruit" // 🍈
+    | "carnitas"; // 🐖
+  extraTaco: boolean;
+  salsa: boolean;
+};
+
+type Sushi = {
+  type: "sushi";
+  protein: "kingSalmon" | "tuna";
+  riceType: RiceType;
+};
+
+type Sandwich = {
+  type: "sandwich";
+  protein:
+    | "chicken" // 🐓
+    | "jackfruit" // 🍈
+    | "portabelloCap"; // 🍄
+  toppings: Topping[];
+};
+
 /*
  * ======================================================
  * TODO: Update LineItem to represent an order from the
  * Monster Foodies Truck.
  * ======================================================*/
-export type LineItem = any;
+export type LineItem = (Taco | Sushi | Sandwich) & { awesomeSauce: boolean };
 
 export interface Order {
   lineItems: LineItem[];
+}
+
+function getTacoPrice({ protein, extraTaco, salsa }: Taco) {
+  let price = 5;
+  if (protein === "carnitas") {
+    price += 2;
+  }
+  if (extraTaco) {
+    price += protein === "carnitas" ? 4 : 3;
+  }
+  if (salsa) {
+    price += 0.5;
+  }
+  return price;
+}
+
+function getSushiPrice({ protein, riceType }: Sushi) {
+  let price = 8;
+  if (protein === "kingSalmon") {
+    price += 2;
+  }
+  if (riceType === "brownRice") {
+    price += 1;
+  }
+  return price;
+}
+
+function getSandwichPrice({ protein, toppings }: Sandwich) {
+  let price = 4;
+  if (protein === "portabelloCap") {
+    price += 2;
+  }
+  if (toppings.length > 1) {
+    price += (toppings.length - 1) * 0.5;
+  }
+  return price;
+}
+
+export function getLineItemPrice(lineItem: LineItem): number {
+  let price = lineItem.awesomeSauce ? 1 : 0;
+  switch (lineItem.type) {
+    case "taco":
+      return price + getTacoPrice(lineItem);
+    case "sushi":
+      return price + getSushiPrice(lineItem);
+    case "sandwich":
+      return price + getSandwichPrice(lineItem);
+  }
 }
 
 /*
@@ -69,10 +142,12 @@ export interface Order {
  * TODO: Implement priceOrder.
  * ======================================================*/
 export function priceOrder(order: Order): number {
-  return 1;
+  return order.lineItems
+    .map(getLineItemPrice)
+    .reduce((price, itemPrice) => price + itemPrice, 0);
 }
 
-/* Monster's foodie truck takes orders on paper slips, 
+/* Monster's foodie truck takes orders on paper slips,
    but sometimes people don't read the menu before they
    order. */
 type PaperLineItem = {
