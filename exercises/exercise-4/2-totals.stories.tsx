d439ccfe3c5d06e4fbda53c78b62f8b4af06d5c8
Will Douglas <willygdouglas@gmail.com>
2019-11-25 13:32:23 -0700

    Complete exercise 4

--- exercises/exercise-4/2-totals.stories.tsx
+++ exercises/exercise-4/2-totals.stories.tsx
@@ -1,15 +1,16 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import { expectToLookLike, ReplaceMe } from "../util/story-helpers";
+import { TotalSection } from "./code";
 import "./styles.css";
-
 
 storiesOf("Exercise 4.2: Totals", module)
   .add("Intro", () => (
     <div>
       <h1>TotalSection</h1>
       <p>
-        Create a component for showing the Subtotal, Tip, and Total at the bottom of the receipt.
+        Create a component for showing the Subtotal, Tip, and Total at the
+        bottom of the receipt.
       </p>
     </div>
   ))
@@ -25,13 +26,15 @@ storiesOf("Exercise 4.2: Totals", module)
       </div>,
 
       // Should be produced by:
-      () => 
-        <ReplaceMe />
+      () => (
+        // <ReplaceMe />
         // with
-        // <TotalSection subtotal={24} tip={3.6} total={27.6} />
+        <TotalSection subtotal={24} tip={3.6} total={27.6} />
+      )
     )
   )
-  .add("Your example 1", () => 
-    <ReplaceMe />
-      // with your own TotalSection example
-  )
+  .add("Your example 1", () => (
+    // <ReplaceMe />
+    // with your own TotalSection example
+    <TotalSection subtotal={24} tip={3.6} total={27.6} />
+  ));
--- exercises/exercise-4/code.tsx
+++ exercises/exercise-4/code.tsx
@@ -38,16 +38,47 @@ export interface IconProps {
   type: FoodStuffs;
 }
 
-
 function descFor(type: FoodStuffs): string {
   switch (type) {
     case "carnitas":
       return "🐖 Carnitas";
-    
-    // TODO: Fill me in by copying and pasting examples from the stories file
 
-    default:
-      throw new Error(`Not implemented case ${type}`);
+    // TODO: Fill me in by copying and pasting examples from the stories file
+    case "taco":
+      return "🌮 Taco";
+    case "sushi":
+      return "🍣 Sushi";
+    case "sandwich":
+      return "🍞 Sandwich";
+    case "chicken":
+      return "🐓 Chicken";
+    case "jackfruit":
+      return "🍈 Jackfruit";
+    case "tuna":
+      return "🐟 Tuna";
+    case "kingSalmon":
+      return "🐟 King Salmon";
+    case "portabelloCap":
+      return "🍄 Portobello Cap";
+    case "brownRice":
+      return "🍘 Brown Rice";
+    case "whiteRice":
+      return "🍙 White Rice";
+    case "cheese":
+      return "🧀 Cheese";
+    case "lettuce":
+      return "🥗 Lettuce";
+    case "tomato":
+      return "🍅 Tomato";
+    case "extraTaco":
+      return "🌮 Extra Taco";
+    case "salsa":
+      return "💃 Salsa";
+    case "awesomeSauce":
+      return "Awesome Sauce";
+
+    // default:
+    //   throw new Error(`Not implemented case ${type}`);
   }
 }
 export const Description: React.SFC<IconProps> = props => {
@@ -56,21 +87,56 @@ export const Description: React.SFC<IconProps> = props => {
 
 type TotalsProps = {
   /* Replace Me */
+  subtotal: number;
+  tip: number;
+  total: number;
 };
 export const TotalSection: React.SFC<TotalsProps> = props => {
   // TODO: Implement me for real. Feel free to copy HTML structure from StaticExample
-  const examplePrice = 3.50;
-  return <div>{3.50.toFixed(2)}</div>;
+  // const examplePrice = 3.5;
+  // return <div>{(3.5).toFixed(2)}</div>;
+
+  return (
+    <div className="total-section">
+      <div className="subtotal-info">
+        Subtotal: ${props.subtotal.toFixed(2)}
+      </div>
+      <div className="subtotal-info">Tip: ${props.tip.toFixed(2)}</div>
+
+      <div className="grand-total">Total: ${props.total.toFixed(2)}</div>
+    </div>
+  );
 };
+
+function getAnnotation(annotation?: string) {
+  switch (annotation) {
+    case "pricey":
+      return "👑";
+    case "nuclear":
+      return "☢️";
+    default:
+      return <>&nbsp;</>;
+  }
+}
 
 export const AdditionSummary: React.SFC<AdditionSummaryProps> = props => (
   // TODO: Implement me for real. Feel free to copy HTML structure from StaticExample
-  <li>An item</li>
+  <li>
+    <div className="crown-marker">{getAnnotation(props.annotation)}</div>
+    <div
+      className={`addition-label${
+        props.additionType === "awesomeSauce" ? " awesome-sauce" : ""
+      }`}
+    >
+      {descFor(props.additionType)}
+    </div>
+  </li>
 );
 
 export const ItemSummary: React.SFC<ItemSummaryProps> = props => (
   // TODO: Implement me for real. Feel free to copy HTML structure from StaticExample
   <div className="line-item">
+    <h3 className="item-kind">{descFor(props.entreeType)}</h3>
     <ul className="addition-list">
       {/* Render an AdditionSummary using each addition as props */}
       {props.additions.map((addition, i) => (
@@ -78,14 +144,28 @@ export const ItemSummary: React.SFC<ItemSummaryProps> = props => (
         <AdditionSummary key={i} {...addition} />
       ))}
     </ul>
+
+    <div className="item-total">Total: ${props.itemTotal}</div>
+
+    {props.mindBlowing && <div className="awesome-sauce-backdrop">😻</div>}
   </div>
 );
 
 export const Receipt: React.SFC<ReceiptProps> = props => (
   <div className="order-summary">
+    <h1>Order Summary</h1>
+
     {props.items.map((item, i) => (
       // Note: key is required by react for arrays of elements
       <ItemSummary key={i} {...item} />
     ))}
+
+    <hr className="divider" />
+
+    <TotalSection
+      subtotal={props.subtotal}
+      tip={props.tip}
+      total={props.total}
+    />
   </div>
 );
